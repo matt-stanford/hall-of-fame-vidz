d8adf328e6fd5e7d5afd909b86c466b2a3658a0d
matt-stanford <mjfstanford@gmail.com>
2020-03-25 22:09:11 +0000

    CRUD using class based views

--- halls/urls.py
+++ halls/urls.py
@@ -5,9 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard', views.dashboard, name='dashboard'),
     # AUTH
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    # HALL
     path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
+    path('halloffame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
+    path('halloffame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
+    path('halloffame/<int:pk>/delete', views.DeleteHall.as_view(), name='delete_hall'),
 ]
--- halls/views.py
+++ halls/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 def home(request):
     return render(request, 'halls/home.html')
+
+
+def dashboard(request):
+    return render(request, 'halls/dashboard.html')
 
 
 class SignUp(generic.CreateView):
@@ -13,9 +18,39 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
 
 class CreateHall(generic.CreateView):
     model = Hall
     fields = ('title',)
     success_url = reverse_lazy('home')
     template_name = 'halls/create_hall.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('dashboard')
+
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    fields = ('title',)
+    success_url = reverse_lazy('dashboard')
+    template_name = 'halls/update_hall.html'
+
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    success_url = reverse_lazy('dashboard')
+    template_name = 'halls/delete_hall.html'
