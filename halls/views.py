a4ef68f9d6347ae709c493a49eab8574a655bbbf
matt-stanford <mjfstanford@gmail.com>
2020-03-28 12:15:05 +0000

    linked to YouTube API

--- halls/views.py
+++ halls/views.py
@@ -6,8 +6,9 @@ from django.contrib.auth import authenticate, login
 from .models import Hall, Video
 from .forms import VideoForm, SearchForm
 from django.http import Http404
-import urllib
 from django.forms.utils import ErrorList
+import requests
+import urllib
 
 YOUTUBE_API_KEY = 'AIzaSyD5biMpq2E5HVymRCSlH3Ti6batIJVZPvg'
 
@@ -24,22 +25,28 @@ def add_video(request, pk):
     search_form = SearchForm()
     hall = Hall.objects.get(pk=pk)
 
-    if hall.user != request.user:
+    if not hall.user == request.user:
         raise Http404
 
     if request.method == 'POST':
-        filled_form = VideoForm(request.POST or None)
-        if filled_form.is_valid():
+        form = VideoForm(request.POST or None)
+        if form.is_valid():
             video = Video()
             video.hall = hall
-            video.url = filled_form.cleaned_data['url']
+            video.url = form.cleaned_data['url']
             parsed_url = urllib.parse.urlparse(video.url)
-            video_id = urllib.parse.parse_qs(parsed_url).get('v')
+            video_id = urllib.parse.parse_qs(parsed_url.query).get('v')
 
             if video_id:
                 video.youtube_id = video_id[0]
-                # video.title = 
+                response = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={ video.youtube_id }&key={YOUTUBE_API_KEY}')
+                json = response.json()
+                video.title = json['items'][0]['snippet']['title']
                 video.save()
+                return redirect('detail_hall', pk)
+            else:
+                errors = form._errors.setdefault('url', ErrorList())
+                errors.append('Needs to be a valid YouTube URL')
 
     context = {
         'form': form,
